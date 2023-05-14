dc84d682c8549b661486bf0b2d9a086f99b55d73
A <mohdadil@iitbhilai.ac.in>
2023-05-14 17:49:22 +0530

    Bugfix-adil

--- script.js
+++ script.js
@@ -4,29 +4,44 @@ console.log("Welcome to spotify");
 let songindex = 0;
 let audioelement = new Audio('1.mp3'); // song play
 let masterplay = document.getElementById('masterplay');
-let MyprogressBar = document.getElementById('MyprogressBar'); 
+let MyprogressBar = document.getElementById('MyprogressBar');
 
 let songs = [
-    {songname: 'mere ram', filepath:'song/1.mp3', coverpath:'covers/2.mp3'},
-    {songname: 'mere ram', filepath:'song/1.mp3', coverpath:'covers/2.mp3'},
-    {songname: 'mere ram', filepath:'song/1.mp3', coverpath:'covers/2.mp3'},
-    {songname: 'mere ram', filepath:'song/1.mp3', coverpath:'covers/2.mp3'},
+    { songname: 'mere ram', filepath: 'song/1.mp3', coverpath: 'covers/2.mp3' },
+    { songname: 'mere ram', filepath: 'song/1.mp3', coverpath: 'covers/2.mp3' },
+    { songname: 'mere ram', filepath: 'song/1.mp3', coverpath: 'covers/2.mp3' },
+    { songname: 'mere ram', filepath: 'song/1.mp3', coverpath: 'covers/2.mp3' },
 ]
 //handle play pause click
-masterplay.addEventListener('click',()=>{
-    if(audioelement.paused || audioelement.currentTime<=0){ 
+// masterplay.addEventListener('click',()=>{
+//     if(audioelement.paused || audioelement.currentTime<=0){ 
+//         audioelement.play();
+//         masterplay.classList.remove('fa-play-circle');
+//         masterplay.classList.add('fa-pause-circle');
+//     }
+//     else{
+//         audioelement.play();
+//         masterplay.classList.remove('fa-pause-circle');
+//         masterplay.classList.add('fa-play-circle'); 
+//     }
+// })
+//listen to events
+
+function myfunction() {
+
+    if (audioelement.paused || audioelement.currentTime <= 0) {
         audioelement.play();
         masterplay.classList.remove('fa-play-circle');
         masterplay.classList.add('fa-pause-circle');
     }
-    else{
-        audioelement.play();
+    else {
+        audioelement.pause();
         masterplay.classList.remove('fa-pause-circle');
-        masterplay.classList.add('fa-play-circle'); 
+        masterplay.classList.add('fa-play-circle');
     }
-})
-//listen to events
-MyprogressBar.addEventListener('timeupdate',()=>{
+}
+
+MyprogressBar.addEventListener('timeupdate', () => {
     console.log('timeupdate');
     //update seekbar
 })
