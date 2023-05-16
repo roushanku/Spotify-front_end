78af26c565abf933ab89bb18c3640f5d7f58d0f3
A <mohdadil@iitbhilai.ac.in>
2023-05-16 18:10:36 +0530

    Just fix

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 console.log("Welcome to spotify");
 
-//intialize the variable
 let songindex = 0;
 let audioelement = new Audio('1.mp3'); // song play
 let masterplay = document.getElementById('masterplay');
@@ -12,20 +11,6 @@ let songs = [
     { songname: 'mere ram', filepath: 'song/1.mp3', coverpath: 'covers/2.mp3' },
     { songname: 'mere ram', filepath: 'song/1.mp3', coverpath: 'covers/2.mp3' },
 ]
-//handle play pause click
-// masterplay.addEventListener('click',()=>{
-//     if(audioelement.paused || audioelement.currentTime<=0){ 
-//         audioelement.play();
-//         masterplay.classList.remove('fa-play-circle');
-//         masterplay.classList.add('fa-pause-circle');
-//     }
-//     else{
-//         audioelement.play();
-//         masterplay.classList.remove('fa-pause-circle');
-//         masterplay.classList.add('fa-play-circle'); 
-//     }
-// })
-//listen to events
 
 function myfunction() {
 
@@ -33,6 +18,7 @@ function myfunction() {
         audioelement.play();
         masterplay.classList.remove('fa-play-circle');
         masterplay.classList.add('fa-pause-circle');
+        console.log(masterplay.classList);
     }
     else {
         audioelement.pause();
@@ -44,5 +30,32 @@ function myfunction() {
 MyprogressBar.addEventListener('timeupdate', () => {
     console.log('timeupdate');
     //update seekbar
+    progress = parseInt((audioelement.currentTime / audioelement.duration) * 100);
+    MyprogressBar.value = progress;
+
 })
 
+//js code to make nav bar
+let nav = document.getElementById('nav');
+let toggle = document.getElementById('toggle');
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+}
+)
+//next song
+let next = document.getElementById('next');
+next.addEventListener('click', () => {
+    songindex = (songindex + 1) % songs.length;
+    loadSong(songindex);
+    audioelement.play();
+})
+
+//graph song
+let previous = document.getElementById('previous');
+previous.addEventListener('click', () => {
+
+    songindex = (songindex - 1 + songs.length) % songs.length;
+    loadSong(songindex);
+    audioelement.play();
+})
